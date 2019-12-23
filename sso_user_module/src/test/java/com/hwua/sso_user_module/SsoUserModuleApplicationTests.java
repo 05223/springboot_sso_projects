268042452fe7795a7a322a2f25ad0753a23f61b7
0522 <2835445407@qq.com>
2019-12-23 15:31:11 +0800

    sso_user_module

---
+++ sso_user_module/src/test/java/com/hwua/sso_user_module/SsoUserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwua.sso_user_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoUserModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
